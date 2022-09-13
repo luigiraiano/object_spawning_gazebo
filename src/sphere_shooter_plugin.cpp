04b6b0501e6980eca43ea0a6c0b95f6085ee9dd3
Luigi Raiano <Luigi.Raiano@iit.it>
2022-09-13 18:39:39 +0200

    plugin to shoot the ball. to be implemented

---
+++ src/sphere_shooter_plugin.cpp
@@ -0,0 +1,16 @@
+#include <ros/ros.h>
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/common/common.hh>
+
+#include <gazebo/physics/World.hh>
+#include <gazebo/physics/Model.hh>
+#include <gazebo/physics/Link.hh>
+#include <gazebo/physics/physics.hh>
+
+#include <ignition/math/Pose3.hh>
+#include <ignition/math/Vector3.hh>
+#include <ignition/math/Quaternion.hh>
+
+#include <iostream>
+
