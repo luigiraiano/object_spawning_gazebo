658d2cdc07314dd9689ada5b65542712adedaff0
Luigi Raiano <Luigi.Raiano@iit.it>
2022-09-15 15:26:06 +0200

    added node for broadcasting over tf the pose of a given target spawned in simulation

--- src/target_pose_publisher_node.cpp
+++ src/target_pose_publisher_node.cpp
@@ -7,8 +7,11 @@
 
 void linkStateCallback(const gazebo_msgs::LinkStates& msgs);
 
-static tf::TransformBroadcaster _br;
+static std::shared_ptr<tf::TransformBroadcaster> _br;
 static std::string _token;
+static std::vector<size_t> _target_idx;
+static std::string _tf_target_name{"target"};
+static std::string _world_name{"world"};
 
 int main(int argc, char **argv)
 {
@@ -21,9 +24,19 @@ int main(int argc, char **argv)
       _token = "target";
   }
 
+  if( !nh_priv.getParam("tf_target_name", _tf_target_name) )
+  {
+      _tf_target_name = "target";
+  }
 
-  std::string sub_name{"/gazebo/link_states"}, pub_name{"/tf"};
+  if( !nh_priv.getParam("world_name", _world_name) )
+  {
+      _world_name = "world";
+  }
+
+  std::string sub_name{"/gazebo/link_states"};
   ros::Subscriber gazebo_link_state_sub = nh.subscribe(sub_name, 10, linkStateCallback);
+  _br.reset(new tf::TransformBroadcaster);
 
   ros::spin();
 
@@ -32,12 +45,24 @@ int main(int argc, char **argv)
 
 void linkStateCallback(const gazebo_msgs::LinkStates& msgs)
 {
+  // Find target using token
+  _target_idx.clear();
+
   for(size_t i=0; i<msgs.name.size(); i++ )
   {
-//    if( find(msgs.name.at(i), _token) != std::string::npos)
-//    {
-        // 1. get link pose
-        // 2. send to tf
-//    }
+    if( msgs.name.at(i).find(_token) != std::string::npos )
+    {
+      _target_idx.push_back(i);
+    }
+  }
+  tf::Transform tf_tranform;
+
+  size_t idx{0};
+  for(size_t i = 0; i<_target_idx.size(); i++)
+  {
+    idx = _target_idx.at(i);
+    tf_tranform.setOrigin( tf::Vector3(msgs.pose.at(idx).position.x, msgs.pose.at(idx).position.y, msgs.pose.at(idx).position.z) );
+    tf_tranform.setRotation( tf::Quaternion( msgs.pose.at(idx).orientation.x, msgs.pose.at(idx).orientation.y, msgs.pose.at(idx).orientation.z, msgs.pose.at(i).orientation.w ) );
+    _br->sendTransform( tf::StampedTransform(tf_tranform, ros::Time::now(), _world_name, _tf_target_name + "_" + std::to_string(i) ) );
   }
 }
